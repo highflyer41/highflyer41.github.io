1c4f3fc20ebb890e0a2418d512b8b8d44175b003
Meng Yang <mengyang41@gmail.com>
2020-04-18 17:34:10 -0400

    update

--- script.js
+++ script.js
@@ -29,23 +29,23 @@ $('#inputbtn').click(() => {
 
             switch(response.current.weather[0].main) {
                 case "Clear": 
-                    $('#picbox').html("<img src='https://media.giphy.com/media/QZz9r30N2RM7DFIXqE/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/QZz9r30N2RM7DFIXqE/source.gif'>")
                     break;
                 case "Thunderstorm": 
-                    $('#picbox').html("<img src='https://media.giphy.com/media/xUOwGoNa2uX6M170d2/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/xUOwGoNa2uX6M170d2/source.gif'>")
                     break;
                 case "Drizzle":
                 case "Rain":
-                    $('#picbox').html("<img src='https://media.giphy.com/media/U7yxnzr21Xhnnb7Zxz/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/U7yxnzr21Xhnnb7Zxz/source.gif'>")
                     break;
                 case "Snow":
-                    $('#picbox').html("<img src='https://media.giphy.com/media/6YNgoTEPs6vZe/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/6YNgoTEPs6vZe/source.gif'>")
                     break;
                 case "Clouds":
-                    $('#picbox').html("<img src='https://media.giphy.com/media/Ke7i5t6QDmDSO82Uga/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/Ke7i5t6QDmDSO82Uga/source.gif'>")
                     break;
                 default:
-                    $('#picbox').html("<img src='https://media.giphy.com/media/xUOxfjsW9fWPqEWouI/source.gif'>")
+                    $('#datebox').append("<img src='https://media.giphy.com/media/xUOxfjsW9fWPqEWouI/source.gif'>")
                     break;
             };
         }); 
